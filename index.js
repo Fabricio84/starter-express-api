4af1eda7f3872fa6607ff8adb222cab29df0a862
x0446891 <fabriciosouza@santos.sp.gov.br>
2022-12-22 16:55:47 -0300

    add routes

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.all('/teste', (req, res) => {
+    console.log("Just got a request!")
+    res.send('hello world!')
+})
+
 app.listen(process.env.PORT || 3000)
